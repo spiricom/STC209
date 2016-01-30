78e9cc7f89d05f488164af18b37cb583a65a61f4
Aatish Bhatia <aatish@gmail.com>
2016-01-30 00:59:23 -0500

    added input mode for hanging mesh

--- structure/2D Hanging Cloth/sketch.js
+++ structure/2D Hanging Cloth/sketch.js
@@ -3,17 +3,23 @@
 // http://natureofcode.com/book/chapter-5-physics-libraries/
 // https://github.com/shiffman/The-Nature-of-Code-Examples-p5.js
 
-// Reference to physics world
-var physics;
+//***********************************************************
+// You can play with the variables in here
 
-var particles = [];
-var springs = [];
+// These set how many particles wide and tall the cloth is:
 var cloth_particles_wide = 15;
 var cloth_particles_tall = 15;
 
+// These set the width and height of the cloth (in pixels):
 var cloth_width = 200;
 var cloth_height = 200;
-var stiffness = 0.15;
+
+var stiffness = 0.15; //spring stiffness
+//***********************************************************
+
+
+var physics; 
+var particles = []; // this will become a 2D array that will store all the particles
 
 var x_spacing = cloth_width/cloth_particles_wide;
 var y_spacing = cloth_height/cloth_particles_tall;
@@ -21,42 +27,48 @@ var y_spacing = cloth_height/cloth_particles_tall;
 var i0 = cloth_particles_wide-1;
 var j0 = cloth_particles_tall-1;
 
+
+
 function setup() {
   createCanvas(640,360);
 
-
-  // Initialize the physics
+  // Initialize the physics world
   physics=new VerletPhysics2D();
   physics.addBehavior(new GravityBehavior(new Vec2D(0,0.5)));
 
-  // Set the world's bounding box
+  // Set the world's bounding box (particles can't leave this box)
   physics.setWorldBounds(new Rect(0,0,width,height));
   
-  // Make particles
+  // Now we'll create a 2d array of particles called particles[i][j]
+  // where i is the column and j is the row of the particle
+  // so e.g. particles[0][0] contains the particle in the 1st row & 1st column
+  // and particles[5][10] contains the particle in the (5+1)th = 6th column & (10+1)th = 11th row 
+  
+  // Go through each column of the cloth..
   for(var i = 0; i<cloth_particles_wide; i++){
 
-    particles[i] = [];
-    springs[i] = [];
+    // this is how we create a 2D array
+    particles[i] = []; 
 
+    // Go through each row of the cloth..
     for(var j = 0; j<cloth_particles_tall; j++){
     
+      // This is where we create the particles
       particles[i][j] = new Particle(new Vec2D(width/2-cloth_width/2+i*x_spacing,j*y_spacing));
-
-      // Anything we make, we have to add into the physics world
+      // And add the particle to the physics workd
       physics.addParticle(particles[i][j]);
 
  
+      // if this isn't the first colum...
       if(i>0){
-        // Make a spring connecting both Particles
-        springs[i-1][j]=new VerletSpring2D(particles[i-1][j],particles[i][j],x_spacing,stiffness);
-        physics.addSpring(springs[i-1][j]);
+        // then make a spring connecting this particle (i,j) to the particle to its left (i-1,j)
+        physics.addSpring(new VerletSpring2D(particles[i][j],particles[i-1][j],x_spacing,stiffness));
       }
+      // if this isn't the first row...
       if(j>0){
-        // Make a spring connecting both Particles
-        springs[i][j-1]=new VerletSpring2D(particles[i][j-1],particles[i][j],y_spacing,stiffness);
-        physics.addSpring(springs[i][j-1]);
+        // also make a spring connecting this particle (i,j) to the particle above it (i,j-1)
+        physics.addSpring(new VerletSpring2D(particles[i][j],particles[i][j-1],y_spacing,stiffness));
       }
-
 
     }
   }
@@ -66,6 +78,9 @@ function setup() {
   particles[cloth_particles_wide-1][0].lock();
 
 }
+
+
+
 
 function draw() {
 
@@ -77,25 +92,28 @@ function draw() {
   // This next bit draws lines between the particles
   stroke(200);
   strokeWeight(2);
-  
 
+  // Again, we iterate through all the particles (columns and rows)
   for(var i = 0; i<cloth_particles_wide; i++){
     for(var j = 0; j<cloth_particles_tall; j++){
 
-      // Display the particles
-      //particles[i][j].display();
-      
+       // if this isn't the first colum...
       if(i>0){
-        line(particles[i-1][j].x,particles[i-1][j].y,particles[i][j].x,particles[i][j].y);
+        // draw a line between this particle (i,j) and the particle to its left (i-1,j)
+        line(particles[i][j].x,particles[i][j].y,particles[i-1][j].x,particles[i-1][j].y);
       }
+      
+      // if this isn't the first row
       if(j>0){
-        line(particles[i][j-1].x,particles[i][j-1].y,particles[i][j].x,particles[i][j].y);
+        // draw a line between this particle (i,j) and the particle above it (i,j-1)
+        line(particles[i][j].x,particles[i][j].y,particles[i][j-1].x,particles[i][j-1].y);
       }
     }
   }
   
 
-  // Move the nearest particle by dragging the mouse
+  // This next part lets you drag the particle closest to the mouse
+  // First, if the mouse is pressed, identify the particle closest to the mouse
   if (mouseIsPressed) {
     for(var i = 0; i<cloth_particles_wide; i++){
       for(var j = 0; j<cloth_particles_tall; j++){
@@ -106,10 +124,11 @@ function draw() {
       }
     }
 
-    particles[i0][j0].lock();    // play with commenting this out
+    // Set this particle's position to be the mouse position
+    particles[i0][j0].lock(); 
     particles[i0][j0].x = mouseX;
     particles[i0][j0].y = mouseY;
-    particles[i0][j0].unlock();  // and this out
+    particles[i0][j0].unlock(); 
 
   } 
 }
